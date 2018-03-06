c2750c312eccd48e79a4ab55fd3b25cece243f0c
Mads Reese <mads.reese@gmail.com>
2018-03-06 14:25:10 +0100

    Initial commit for async logger

---
+++ Logger/Logger/AsyncLogger.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logger
+{
+    class AsyncLogger : ILogger
+    {
+        //private readonly BlockingCollection<LogMessage> _pendingMessages;
+        private readonly Type _loggerFor;
+        //private readonly IThreadAdapter _threadAdapter
+
+        public void Critical(string message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Info(string message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Warning(string message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

ec238815e8a8471466e80e980f2714a8da0850bf
Mads Reese <mads.reese@gmail.com>
2018-03-13 19:24:55 +0100

    Added pi calculation Borrowed from csharphelper

--- CalculateService/CalcPI/CalcService.svc.cs
+++ CalculateService/CalcPI/CalcService.svc.cs
@@ -8,9 +8,9 @@ using System.Text;
 
 namespace CalcPI
 {
-    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
-    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
-    public class Service1 : IService1
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "CalcService" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select CalcService.svc or CalcService.svc.cs at the Solution Explorer and start debugging.
+    public class CalcService : ICalcService
     {
 
         public decimal PI(string value)
---
+++ CalculateService/NumberCalculator/CalcPi.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberCalculator
+{
+    class CalcPi
+    {
+        //Nilakantha series - shamelessly borrowed from http://csharphelper.com/blog/2015/03/approximate-pi-using-several-different-methods-in-c/
+        public double CalculatePrime(int num_terms)
+        {
+
+            double result = 0;
+            double sign = 1;
+            for (int i = 0; i < num_terms; i++)
+            {
+                result += sign / (2 * i + 2) / (2 * i + 3) / (2 * i + 4);
+                sign = -sign;
+            }
+            return 3 + result * 4;
+        }
+    }   
+}
